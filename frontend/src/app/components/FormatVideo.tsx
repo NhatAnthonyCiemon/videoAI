43b850ae179f9c37d3e28624dbf13ce1812d06b0
22120197 <nguyenvinhluong242004@gmail.com>
2025-05-06 15:11:58 +0700

    Done: Page Edit Video

--- frontend/src/app/components/FormatVideo.tsx
+++ frontend/src/app/components/FormatVideo.tsx
@@ -1,20 +1,11 @@
 "use client";
 import { useState } from "react";
-
-import { AlignCenterIcon, AlignJustify, AlignLeft, AlignRight } from "lucide-react";
+import TabText from "./TabText";
+import TabSticker from "./TabSticker";
+import TabMusic from "./TabMusic";
 
 export default function FormatVideo() {
-  const [fontFamily, setFontFamily] = useState("Arial");
-  const [fontSize, setFontSize] = useState(16);
-  const [bold, setBold] = useState(false);
-  const [italic, setItalic] = useState(false);
-  const [underline, setUnderline] = useState(false);
-  const [textAlignment, setTextAlignment] = useState("left");
-  const [textFill, setTextFill] = useState("top");
-  const [textEffect, setTextEffect] = useState("none");
-  const [textColor, setTextColor] = useState("#000000");
   const [tab, setTab] = useState("sticker");
-
 
   return (
     <div className="w-[360px] h-full bg-white border-l-1 border-gray-700 flex flex-col">
@@ -38,132 +29,12 @@ export default function FormatVideo() {
           >
             Music
           </button>
-
-
         </div>
       </div>
-
-      <div className="space-y-4 text-xl pt-4 bg-white overflow-y-auto p-4">
-        <h2 className="font-bold">Subtitle Settings</h2>
-
-        <div className="p-2">
-          <div className="flex flex-col justify-between mb-5">
-            <span>Text Alignment</span>
-            <div className="flex gap-2 mt-2">
-              <AlignLeft className={`border cursor-pointer rounded-md p-1 ${textAlignment === 'left' ? "bg-gray-300" : ""}`} size={28} color="black" onClick={() => setTextAlignment('left')}/>
-              <AlignCenterIcon className={`border cursor-pointer rounded-md p-1 ${textAlignment === 'center' ? "bg-gray-300" : ""}`} size={28} color="black" onClick={() => setTextAlignment('center')}/>
-              <AlignRight className={`border cursor-pointer rounded-md p-1 ${textAlignment === 'right' ? "bg-gray-300" : ""}`} size={28} color="black" onClick={() => setTextAlignment('right')}/>
-              <AlignJustify className={`border cursor-pointer rounded-md p-1 ${textAlignment === 'justify' ? "bg-gray-300" : ""}`} size={28} color="black" onClick={() => setTextAlignment('justify')}/>
-            </div>
-          </div>
-
-          <div className="mt-10">
-            <label className="block font-medium mb-1">Font</label>
-            <div className="flex gap-2 text-2xl">
-              <button
-                className={`font-bold cursor-pointer btn w-12 h-12 border p-1 rounded ${bold ? "bg-gray-300" : ""}`}
-                onClick={() => setBold(!bold)}
-              >
-                B
-              </button>
-              <button
-                className={`italic cursor-pointer w-12 h-12 border p-1 rounded ${italic ? "bg-gray-300" : ""}`}
-                onClick={() => setItalic(!italic)}
-              >
-                I
-              </button>
-              <button
-                className={`underline cursor-pointer w-12 h-12 border p-1 rounded ${underline ? "bg-gray-300" : ""}`}
-                onClick={() => setUnderline(!underline)}
-              >
-                U
-              </button>
-            </div>
-            <div className="flex mt-4">
-              <select
-                className="border p-1 rounded w-full mr-2"
-                value={fontFamily}
-                onChange={(e) => setFontFamily(e.target.value)}
-              >
-                <option>Arial</option>
-                <option>Roboto</option>
-              </select>
-              <select
-                className="border p-1 rounded w-[60px]"
-                value={fontSize}
-                onChange={(e) => setFontSize(parseInt(e.target.value))}
-              >
-                <option>16</option>
-                <option>18</option>
-                <option>20</option>
-              </select>
-            </div>
-            <label className="block font-medium mt-10">Color</label>
-            <div className="flex mt-2">
-              <input
-                type="color"
-                value={textColor}
-                onChange={(e) => setTextColor(e.target.value)}
-                className="border p-1 rounded w-full mr-2 cursor-pointer"
-              />
-              <input
-                type="text"
-                value={textColor}
-                onChange={(e) => setTextColor(e.target.value)}
-                className="border p-1 rounded w-[60px]"
-              />
-            </div>
-          </div>
-
-          <div className="mt-10">
-            <label className="block font-medium mb-1">Fill</label>
-            <div className="flex gap-2 text-xl">
-              <button
-                className={`cursor-pointer btn w-30 h-12 border p-1 rounded ${textFill === 'top' ? "bg-gray-300" : ""}`}
-                onClick={() => setTextFill('top')}
-              >
-                Top
-              </button>
-              <button
-                className={`cursor-pointer btn w-30 h-12 border p-1 rounded ${textFill === 'middle' ? "bg-gray-300" : ""}`}
-                onClick={() => setTextFill('middle')}
-              >
-                Middle
-              </button>
-              <button
-                className={`cursor-pointer btn w-30 h-12 border p-1 rounded ${textFill === 'bottom' ? "bg-gray-300" : ""}`}
-                onClick={() => setTextFill('bottom')}
-              >
-                Bottom
-              </button>
-            </div>
-          </div>
-
-          <div className="mt-10">
-            <label className="block font-medium mb-1">Text Effects</label>
-            <div className="flex gap-2 text-xl">
-              <button
-                className={`cursor-pointer btn w-30 h-12 border p-1 rounded ${textEffect === 'none' ? "bg-gray-300" : ""}`}
-                onClick={() => setTextEffect('none')}
-              >
-                None
-              </button>
-              <button
-                className={`cursor-pointer btn w-30 h-12 border p-1 rounded ${textEffect === 'shadow' ? "bg-gray-300" : ""}`}
-                onClick={() => setTextEffect('shadow')}
-              >
-                Shadow
-              </button>
-              <button
-                className={`cursor-pointer btn w-30 h-12 border p-1 rounded ${textEffect === 'outline' ? "bg-gray-300" : ""}`}
-                onClick={() => setTextEffect('outline')}
-              >
-                Outline
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      
+      {tab === "text" && <TabText />}
+      {tab === "sticker" && <TabSticker />}
+      {tab === "music" && <TabMusic />}
     </div>
   );
 }
--- frontend/src/app/components/VideoPreview.tsx
+++ frontend/src/app/components/VideoPreview.tsx
@@ -1,40 +1,165 @@
-import { Play } from "lucide-react";
+"use client";
+import {
+    Play,
+    Pause,
+    Volume2 as Speaker,
+    VolumeX as SpeakerOff,
+} from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 export default function VideoPreview() {
-    const content =
-        "Figma ipsum export pencil scrolling edit ellipse bullet edit hand blur text mask opacity layout blur shadow union layout star device effect prototype fill auto rectangle list component frame ellipse strikethrough bold align figma select slice pencil vertical main bullet arrange arrange plugin frame vector edit rotate scrolling invite connection plugin plugin main project boolean asset pixel invite bullet vector stroke device opacity object figjam clip text image text inspect object edit italic vector bold effect scale invite rectangle pen underline vertical scrolling draft main line flatten reesizing layout duplicate overflow invite figjam layer asset subtract move export italic vector.";
+    const url =
+        "https://res.cloudinary.com/dphytbuah/video/upload/Video_tiktok/4a66e6c6-cc89-4e30-8ca6-1284f33fea9e.mp4";
+
+    const subtitles = [
+        { start: 0, end: 5, text: "Xin chào, đây là một video mẫu." },
+        { start: 5, end: 10, text: "Tiếp tục với nội dung kế tiếp." },
+        { start: 10, end: 15, text: "Cảm ơn bạn đã theo dõi." },
+    ];
+
+    const content = "Figma ipsum export pencil scrolling edit ellipse bullet edit hand blur text mask opacity layout blur shadow union layout star device effect prototype fill auto rectangle list component frame ellipse strikethrough bold align figma select slice pencil vertical main bullet arrange arrange plugin frame vector edit rotate scrolling invite connection plugin plugin main project boolean asset pixel invite bullet vector stroke device opacity object figjam clip text image text inspect object edit italic vector bold effect scale invite rectangle pen underline vertical scrolling draft main line flatten reesizing layout duplicate overflow invite figjam layer asset subtract move export italic vector.";
+
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [duration, setDuration] = useState(0);
+    const [currentSubtitle, setCurrentSubtitle] = useState("");
+    const [volume, setVolume] = useState(1); // full volume
+
+    const togglePlay = () => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        if (isPlaying) {
+            video.pause();
+        } else {
+            video.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
+    const handleTimeUpdate = () => {
+        const time = videoRef.current?.currentTime || 0;
+        setCurrentTime(time);
+
+        const activeSub = subtitles.find(
+            (sub) => time >= sub.start && time <= sub.end
+        );
+        setCurrentSubtitle(activeSub ? activeSub.text : "");
+    };
+
+    const handleLoadedMetadata = () => {
+        if (videoRef.current) {
+            setDuration(videoRef.current.duration);
+        }
+    };
+
+    const handleSeek = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const time = parseFloat(e.target.value);
+        if (videoRef.current) {
+            videoRef.current.currentTime = time;
+        }
+        setCurrentTime(time);
+    };
+
+    const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const vol = parseFloat(e.target.value);
+        setVolume(vol);
+        if (videoRef.current) {
+            videoRef.current.volume = vol;
+        }
+    };
+
+    const formatTime = (time: number) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60)
+            .toString()
+            .padStart(2, "0");
+        return `${minutes}:${seconds}`;
+    };
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.volume = volume;
+        }
+    }, [volume]);
 
     return (
         <div className="flex-1 flex flex-col pt-5 px-4 py-6">
             <div className="w-full bg-black flex justify-center overflow-hidden rounded-lg shadow relative">
                 <video
-                    src="/sample.mp4"
+                    ref={videoRef}
+                    src={url}
+                    onTimeUpdate={handleTimeUpdate}
+                    onLoadedMetadata={handleLoadedMetadata}
                     className="w-full object-cover"
+                    loop
                 >
                     Your browser does not support the video tag.
                 </video>
 
-                <p className="absolute bottom-7 justify-center text-white text-sm bg-black bg-opacity-50 px-2 py-1 rounded">
-                    Subtitle here...
-                </p>
+                {currentSubtitle && (
+                    <p className="absolute bottom-7 justify-center text-white text-sm bg-black bg-opacity-50 px-2 py-1 rounded">
+                        {currentSubtitle}
+                    </p>
+                )}
 
-                <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-                    <Play size={40} className="text-white opacity-60" />
+                {!isPlaying && (
+                    <div
+                        className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 cursor-pointer"
+                        onClick={togglePlay}
+                    >
+                        <Play size={40} className="text-white opacity-80" />
+                    </div>
+                )}
+            </div>
+
+            <div className="w-full mt-4 flex items-center gap-2">
+                <button
+                    onClick={togglePlay}
+                    className="text-blue-600 hover:scale-110 transition"
+                >
+                    {isPlaying ? <Pause size={24} /> : <Play size={24} />}
+                </button>
+
+                <span className="text-base">{formatTime(currentTime)}</span>
+                <span>/</span>
+                <span className="text-base">{formatTime(duration)}</span>
+
+                <input
+                    type="range"
+                    min={0}
+                    max={duration}
+                    step={0.1}
+                    value={currentTime}
+                    onChange={handleSeek}
+                    className="flex-1 accent-blue-600 cursor-pointer mx-2"
+                />
+
+                {/* Volume Control */}
+                <div className="relative group ml-2">
+                    <div className="cursor-pointer">
+                        {volume > 0 ? (
+                            <Speaker size={20} className="text-blue-600" />
+                        ) : (
+                            <SpeakerOff size={20} className="text-blue-600" />
+                        )}
+                    </div>
+
+                    <input
+                        type="range"
+                        min={0}
+                        max={1}
+                        step={0.01}
+                        value={volume}
+                        onChange={handleVolumeChange}
+                        className="cursor-pointer absolute bottom-24 left-7 -translate-x-1/2 w-30 rotate-[-90deg] origin-bottom group-hover:opacity-100 opacity-0 transition-opacity duration-300"
+                    />
                 </div>
             </div>
 
-            <div className="w-full mt-4">
-                <div className="flex items-center gap-2">
-                    <Play size={24} className="text-blue-600" />
-                    <span className="text-base">0:00</span>
-                    <span>/</span>
-                    <span className="text-base">0:30</span>
-                    <input type="range" className="flex-1" />
-                </div>
-            </div>
 
             <p className="mt-5 text-2xl font-semibold">Transcript</p>
-            <p className="text-xl text-gray-700">{content}</p>
+            <p className="text-xl text-gray-700 p-2">{content}</p>
         </div>
     );
 }

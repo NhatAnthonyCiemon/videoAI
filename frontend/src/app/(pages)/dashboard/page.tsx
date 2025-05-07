de8664af0b523bdc4dd30afc0c54aceac5c9c7dd
TNMonster <nhatmonsterhack@gmail.com>
2025-05-07 13:44:28 +0700

    chinh sua cac page

--- frontend/src/app/(pages)/dashboard/page.tsx
+++ frontend/src/app/(pages)/dashboard/page.tsx
@@ -1,14 +1,18 @@
+import Header from "@/components/layout/header";
 import Category from "./Catery";
 import Search from "./Search";
 import Videos from "./Videos";
 
 function Dashboard() {
     return (
-        <div className="py-[50px] w-[1280px] mx-auto">
-            <Search />
-            <Category />
-            <Videos />
-        </div>
+        <>
+            <Header />
+            <div className="py-[50px] w-[1280px] mx-auto">
+                <Search />
+                <Category />
+                <Videos />
+            </div>
+        </>
     );
 }
 

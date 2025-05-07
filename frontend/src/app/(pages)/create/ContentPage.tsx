98d5108205c14883fec1000835cd32daaea2964d
TNMonster <nhatmonsterhack@gmail.com>
2025-05-07 13:44:28 +0700

    chinh sua cac page

--- frontend/src/app/(pages)/create/ContentPage.tsx
+++ frontend/src/app/(pages)/create/ContentPage.tsx
@@ -5,6 +5,15 @@ import CreateScript from "./CreateScript";
 import CreateVideo from "./CreateVideo";
 import CreateTop from "./CreateTop";
 function ContentPage() {
+    React.useEffect(() => {
+        document.documentElement.classList.add("scrollbar-gutter-stable");
+        return () => {
+            document.documentElement.classList.remove(
+                "scrollbar-gutter-stable"
+            );
+        };
+    }, []);
+
     const [whichActive, setWhichActive] = React.useState(0);
     return (
         <div className="w-[1280px] mx-auto overflow-hidden">

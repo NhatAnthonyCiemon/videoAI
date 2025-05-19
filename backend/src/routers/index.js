ae25d41dbe10968205396fce59ec97179edf7f68
22120209 <minhpqdp02@gmail.com>
2025-05-19 22:02:19 +0700

    routers

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -1,10 +1,10 @@
-const userRouter = require("../app/user/route");
+import userRouter from "../app/user/route.js";
 
 function route(app) {
-    app.use("/", (req, res) => {
-        res.send("Hello World");
-    });
     app.use("/user", userRouter);
+    app.use("/", (req, res) => {
+        res.json({ message: "Hello World" }); // Trả về JSON thay vì chuỗi văn bản
+    });
 }
 
-module.exports = route;
+export default route;

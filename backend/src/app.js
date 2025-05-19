b89456a2df823508fa5b7be0b60fd61972d0d2f6
22120209 <minhpqdp02@gmail.com>
2025-05-19 22:01:04 +0700

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,30 @@
-const express = require("express");
-const route = require("./routers/index");
-const dotenv = require("dotenv");
-//const passport = require("./config/passport");
+import express from "express";
+import route from "./routers/index.js";
+import dotenv from "dotenv";
+import cors from "cors"; // Import middleware CORS
 
 dotenv.config();
 
 const app = express();
+
+// Cấu hình CORS
+app.use(cors({
+    origin: "*", // Cho phép tất cả các domain (có thể thay bằng domain cụ thể)
+    methods: ["GET", "POST", "PUT", "DELETE"], // Các phương thức được phép
+    allowedHeaders: ["Content-Type", "Authorization"], // Các header được phép
+}));
 
 app.use(express.static("./src/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 route(app);
-//Middleware handle errors
+
+// Middleware handle errors
 app.listen(process.env.PORT, () => {
     console.log(
-        `Example app listening at http://localhost:${process.env.port}`
+        `Example app listening at http://localhost:${process.env.PORT}`
     );
 });
 
-module.exports = app;
+export default app;

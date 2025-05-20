16c0f23d20e5df01f854e54bdeb26d098ffcbf95
22120197 <nguyenvinhluong242004@gmail.com>
2025-05-21 00:42:51 +0700

    add music and sticker

--- frontend/src/app/edit/EditVideo.tsx
+++ frontend/src/app/edit/EditVideo.tsx
@@ -7,7 +7,56 @@ import SubtitleSetting from "../../components/ui/SubtitleSetting";
 import MusicSetting from "../../components/ui/MusicSetting";
 import StickerSetting from "../../components/ui/StickerSetting";
 
-const InitialSubtitles = [
+interface Music {
+    id: number | null;
+    data: string;
+    start: number;
+    end: number;
+    volume: number;
+}
+
+interface Sticker {
+    id: number | null;
+    data: string;
+    start: number;
+    end: number;
+    style: {
+        width: number;
+        height: number;
+        rotate: number;
+        position: {
+            x: number;
+            y: number;
+        };
+    };
+}
+
+interface Subtitle {
+    text: string;
+    start: number;
+    end: number;
+    style: {
+        width: number;
+        position: string;
+        fontSize: number;
+        fontColor: string;
+        backgroundColor: string;
+        fontStyle: string[];
+        alignment: string;
+        shadow: {
+            color: string;
+            blur: number;
+            offsetX: number;
+            offsetY: number;
+        };
+        outline: {
+            color: string;
+            width: number;
+        };
+    };
+}
+
+const InitialSubtitles: Subtitle[] = [
     {
         text: "Đây là phụ đề.",
         start: 0,
@@ -34,7 +83,7 @@ const InitialSubtitles = [
     },
 ];
 
-function AddNewSubtitle(text: string, currentList: any[] = []) {
+function AddNewSubtitle(text: string, currentList: Subtitle[] = []): Subtitle {
     const lastSubtitle = currentList[currentList.length - 1];
     const startTime = lastSubtitle ? lastSubtitle.end : 0;
 
@@ -64,20 +113,85 @@ function AddNewSubtitle(text: string, currentList: any[] = []) {
     };
 }
 
+const InitialMusics: Music[] = [
+    {
+        id: 0,
+        data: "https://res.cloudinary.com/dphytbuah/video/upload/v1747737999/nh%E1%BA%A1c_n%E1%BB%81n_mp3cut.net_rdtjlc.mp3",
+        start: 0,
+        end: 3.984,
+        volume: 0.5
+    },
+];
+
+function AddNewMusic(data: string, isFromSystem: boolean, currentList: Music[] = []): Music {
+    const lastMusic = currentList[currentList.length - 1];
+    const startTime = lastMusic ? lastMusic.end : 0;
+
+    return {
+        id: isFromSystem ? currentList.length : null,
+        data,
+        start: startTime,
+        end: startTime + 1,
+        volume: 0.5
+    };
+}
+
+const InitialStickers: Sticker[] = [
+    {
+        id: 0,
+        data: "https://res.cloudinary.com/dphytbuah/image/upload/v1747738119/images-removebg-preview_y36zfk.png",
+        start: 0,
+        end: 3.984,
+        style: {
+            width: 100,
+            height: 100,
+            rotate: 0,
+            position: {
+                x: 200,
+                y: 200
+            }
+        }
+    },
+];
+
+function AddNewSticker(data: string, isFromSystem: boolean, currentList: Sticker[] = []): Sticker {
+    const lastSticker = currentList[currentList.length - 1];
+    const startTime = lastSticker ? lastSticker.end : 0;
+
+    return {
+        id: isFromSystem ? currentList.length : null,
+        data,
+        start: startTime,
+        end: startTime + 1,
+        style: {
+            width: 100,
+            height: 100,
+            rotate: 0,
+            position: {
+                x: 200,
+                y: 200
+            }
+        }
+    };
+}
+
 export default function EditVideo() {
     const [selectedTool, setSelectedTool] = useState("subtitles");
-    const [tab, setTab] = useState("sticker");
-    const [subtitles, setSubtitles] = useState(InitialSubtitles);
-    const [idxText, setIdxText] = useState(subtitles.length > 0 ? subtitles.length - 1 : -1);
+    const [tab, setTab] = useState<string>("sticker");
+    const [subtitles, setSubtitles] = useState<Subtitle[]>(InitialSubtitles);
+    const [musics, setMusics] = useState<Music[]>(InitialMusics);
+    const [stickers, setStickers] = useState<Sticker[]>(InitialStickers);
+    const [idxText, setIdxText] = useState<number>(subtitles.length > 0 ? subtitles.length - 1 : -1);
+    const [idxMusic, setIdxMusic] = useState<number>(musics.length > 0 ? musics.length - 1 : -1);
+    const [idxSticker, setIdxSticker] = useState<number>(stickers.length > 0 ? stickers.length - 1 : -1);
 
     const handleAddSubtitle = (text: string) => {
         const newSubtitle = AddNewSubtitle(text, subtitles);
         setSubtitles([...subtitles, newSubtitle]);
-        setIdxText(idxText + 1);
+        setIdxText(subtitles.length);
     };
 
-    const handleUpdateSubtitle = (updatedSubtitle: any) => {
-        // console.log(updatedSubtitle)
+    const handleUpdateSubtitle = (updatedSubtitle: Subtitle) => {
         const updatedList = [...subtitles];
         updatedList[idxText] = updatedSubtitle;
         setSubtitles(updatedList);
@@ -86,10 +200,47 @@ export default function EditVideo() {
     const handleDeleteSubtitle = (index: number) => {
         const updatedList = subtitles.filter((_, i) => i !== index);
         setSubtitles(updatedList);
+        setIdxText(updatedList.length > 0 ? updatedList.length - 1 : -1);
+    };
+
+    const handleAddMusic = (data: string, isFromSystem: boolean) => {
+        const newMusic = AddNewMusic(data, isFromSystem, musics);
+        setMusics([...musics, newMusic]);
+        setIdxMusic(musics.length);
+    };
+
+    const handleUpdateMusic = (updatedMusic: Music) => {
+        const updatedList = [...musics];
+        updatedList[idxMusic] = updatedMusic;
+        setMusics(updatedList);
+    };
+
+    const handleDeleteMusic = (index: number) => {
+        const updatedList = musics.filter((_, i) => i !== index);
+        setMusics(updatedList);
+        setIdxMusic(updatedList.length > 0 ? updatedList.length - 1 : -1);
+    };
+
+    const handleAddSticker = (data: string, isFromSystem: boolean) => {
+        const newSticker = AddNewSticker(data, isFromSystem, stickers);
+        setStickers([...stickers, newSticker]);
+        setIdxSticker(stickers.length);
+    };
+
+    const handleUpdateSticker = (updatedSticker: Sticker) => {
+        const updatedList = [...stickers];
+        updatedList[idxSticker] = updatedSticker;
+        setStickers(updatedList);
+    };
+
+    const handleDeleteSticker = (index: number) => {
+        const updatedList = stickers.filter((_, i) => i !== index);
+        setStickers(updatedList);
+        setIdxSticker(updatedList.length > 0 ? updatedList.length - 1 : -1);
     };
 
     return (
-        <div className="flex h-full w-full pt-30 ">
+        <div className="flex h-full w-full pt-30">
             <SideBar selected={selectedTool} onSelect={setSelectedTool} />
             <div className="bg-white w-[400px]">
                 {selectedTool === "subtitles" && (
@@ -102,15 +253,42 @@ export default function EditVideo() {
                         setIdxText={setIdxText}
                     />
                 )}
-                {selectedTool === "music" && <MusicSetting setTab={setTab} />}
+                {selectedTool === "music" && (
+                    <MusicSetting
+                        musics={musics}
+                        onAdd={handleAddMusic}
+                        onUpdate={handleUpdateMusic}
+                        onDelete={handleDeleteMusic}
+                        idxMusic={idxMusic}
+                        setIdxMusic={setIdxMusic}
+                        setTab={setTab}
+                    />
+                )}
                 {selectedTool === "sticker" && (
-                    <StickerSetting setTab={setTab} />
+                    <StickerSetting
+                        stickers={stickers}
+                        onAdd={handleAddSticker}
+                        onUpdate={handleUpdateSticker}
+                        onDelete={handleDeleteSticker}
+                        idxSticker={idxSticker}
+                        setIdxSticker={setIdxSticker}
+                        setTab={setTab}
+                    />
                 )}
             </div>
             <div className="flex-1 overflow-y-auto max-h-screen custom-scroll">
                 <VideoPreview />
             </div>
-            <FormatVideo tab={selectedTool} setTab={setSelectedTool} subtitle={subtitles[idxText]} onUpdate={handleUpdateSubtitle}/>
+            <FormatVideo 
+                tab={selectedTool} 
+                setTab={setSelectedTool} 
+                subtitle={subtitles[idxText]} 
+                music={musics[idxMusic]} 
+                sticker={stickers[idxSticker]} 
+                onUpdateSubtitle={handleUpdateSubtitle}
+                onUpdateMusic={handleUpdateMusic}
+                onUpdateSticker={handleUpdateSticker}
+            />
         </div>
     );
 }
